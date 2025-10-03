d5520054ceacd6f88e402818a8d136fd11d77fd7
Eltjo Timmerman <eltjo@example.com>
2025-10-03 09:54:39 +0200

    test(web): define TextEncoder/TextDecoder before undici

--- apps/web/jest.setup.ts
+++ apps/web/jest.setup.ts
@@ -1,9 +1,9 @@
+import { TextEncoder, TextDecoder } from 'node:util';
+Object.defineProperty(globalThis, 'TextEncoder', { value: TextEncoder, writable: true });
+Object.defineProperty(globalThis, 'TextDecoder', { value: TextDecoder, writable: true });
 import '@testing-library/jest-dom';
 import { fetch, Request, Response, Headers } from 'undici';
-import { TextEncoder, TextDecoder } from 'node:util';
 Object.defineProperty(globalThis, 'fetch', { value: fetch, writable: true });
 Object.defineProperty(globalThis, 'Request', { value: Request, writable: true });
 Object.defineProperty(globalThis, 'Response', { value: Response, writable: true });
 Object.defineProperty(globalThis, 'Headers', { value: Headers, writable: true });
-Object.defineProperty(globalThis, 'TextEncoder', { value: TextEncoder, writable: true });
-Object.defineProperty(globalThis, 'TextDecoder', { value: TextDecoder, writable: true });
